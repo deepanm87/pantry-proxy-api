d3fadc59991f244f27d99986a96d9e5d29ceb09d
deepanm87 <deepan.murugan29@gmail.com>
2025-02-21 14:12:30 -0500

    update server.js for post, put, delete

--- server.js
+++ server.js
@@ -10,8 +10,55 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const PANTRY_ID = process.env.PANTRY_ID
 
+const PANTRY_API_BASE_URL = `https://getpantry.cloud/apiv1/pantry/${PANTRY_ID}/basket`
+
+app.use(cors())
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/:basketName', async (req, res) => {
+    const {basketName} = req.params
+    try {
+        const response = await axios.get(`${PANTRY_API_BASE_URL}/${basketName}`)
+        res.json(response.data)
+    } catch(e) {
+        res.status(500).json({error: e.message})
+    }
+})
+
+app.post('/:basketName', async (req, res) => {
+    const {basketName} = req.params
+    const newData = req.body
+    try {
+        const response = await axios.post(`${PANTRY_API_BASE_URL}/${basketName}`, newData)
+        res.json(response.data)
+    } catch(e) {
+        res.status(500).json({error: e.message})
+    }
+})
+
+app.put('/:basketName', async (req, res) => {
+    const {basketName} = req.params
+    const newData = req.body
+    try {
+        const response = await axios.put(`${PANTRY_API_BASE_URL}/${basketName}`, newData)
+        res.json(response.data)
+    } catch(e) {
+        res.status(500).json({error: e.message})
+    }
+})
+
+app.delete('/:basketName', async (req, res) => {
+    const {basketName} = req.params
+    try {
+        const response = await axios.get(`${PANTRY_API_BASE_URL}/${basketName}`)
+        res.json({ message: `Basket ${basketName} cleared successfully.`})
+    } catch(e) {
+        res.status(500).json({error: e.message})
+    }
 })
 
 app.listen(PORT, () => {
